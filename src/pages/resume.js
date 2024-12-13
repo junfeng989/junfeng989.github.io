4bc9c6e12330d85f5828e32b79e3b93243acbf44
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2024-12-13 20:46:48 +0800

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -13,7 +13,7 @@ const ResumePage = () => (
       
       <h2>学术成果与荣誉</h2>
       <ul>
-        <li>在信息安全领域发表重要学术论文40余篇，包括IEEE TDSC、IEEE TC、AAAI、IEEE TII、ACM TOIT、IEEE TBD、IEEE TSUSC、IEEE IOT、INS、IEEE WCM等，
+        <li>发表重要学术论文40余篇，包括IEEE TDSC、IEEE TC、AAAI、IEEE TII、ACM TOIT、IEEE TBD、IEEE TSUSC、IEEE IOT、INS、IEEE WCM等，
             其中第一作者IEEE/ACM Transactions论文10余篇</li>
         <li>入选国家级博士后人才计划</li>
         <li>IEEE TCSC杰出博士论文奖获得者</li>
@@ -28,7 +28,7 @@ const ResumePage = () => (
         <li>参与国家重点研发计划网络空间安全重点专项、国家自然科学基金重点项目</li>
         <li>担任国际会议IEEE ATC 2024、ICBC 2023、IEEE PriComp 2022、IEEE DependSys 2021、IEEE DependSys 2020、SpaCCS 2019的程序委员会主席</li>
         <li>担任CCF A类会议ICML 2025, WWW 2025 (ACM TheWebConf 2025), ICLR 2025, AAAI 2025, NeurIPS 2024, ACM MM 2024, AAAI 2024, AAAI 2023等顶级会议程序委员会委员或审稿人</li>
-        <li>担任20多个国际学术期刊例如CCF A类期刊IEEE TDSC、IEEE TIFS、IEEE TC、ACM TOSEM的审稿专家</li>
+        <li>担任20多个国际学术期刊例如IEEE TDSC、IEEE TIFS、IEEE TC、ACM TOSEM的审稿专家</li>
         <li>担任中国中文信息学会大数据安全与隐私计算专委会委员、中国计算机学会嵌入式系统专委会执行委员</li>
         <li>担任国家科技专家库专家；担任国家自然科学基金评审专家；担任加拿大国家自然科学基金评审专家</li>
         <li>担任湖北省科技厅专家库专家、浙江省科技专家库专家、北京市自然科学基金评审专家等科技项目评审专家</li>
