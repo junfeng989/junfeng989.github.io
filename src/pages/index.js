689dddafd0cf3088234252b0cdb80acc38bc69b7
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2024-12-13 20:57:42 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,6 +62,7 @@ const IndexPage = () => (
             <li>2024年12月，立项两项大模型安全方向课题</li>
             <li>2024年12月，针对联邦学习的后门攻击论文被CCF A类会议AAAI 2025录用</li>
             <li>2024年12月，安全神经网络推理论文被CCF A类期刊IEEE TIFS录用</li>
+            <li>2024年9月，CCF A类期刊IEEE TC差分隐私深度学习论文入选了ESI高被引论文</li>
             <li>{/*2024年12月*/}</li>
           </ul>
         </section>
