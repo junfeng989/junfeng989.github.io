b222fe7b9f16bacef73df9c708af1bac194be61e
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2024-12-13 20:51:11 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,12 +134,12 @@ const contentStyles = {
   alignItems: "flex-start",
 }
 const imageStyles = {
-  borderRadius: "10%",           // 圆形边框
+  borderRadius: "5%",           // 圆形边框
   boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",  // 添加阴影
   marginBottom: "20px",          // 图片与文字之间的间隔
-  border: "5px solid #007acc",   // 椭圆形边框，颜色为蓝色
+  border: "1px solid #007acc",   // 椭圆形边框，颜色为蓝色
   objectFit: "cover",            // 确保图片内容适配
   width: "140px",                // 固定宽度
-  height: "180px",               // 固定高度，制造椭圆效果
+  height: "170px",               // 固定高度，制造椭圆效果
 }
 export default IndexPage
