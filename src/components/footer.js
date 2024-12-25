155825b6f3b89f0e839a9c437a80867ce105f7f2
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2024-12-25 21:32:40 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => (
   <footer style={footerStyles}>
-    <p>&copy; 2024 华中科技大学</p>
+    <p>&copy; 2024 冯君</p>
     <p>
       <a href="https://www.gatsbyjs.com" target="_blank" rel="noopener noreferrer">
         {/*Powered by Gatsby*/}
