6a4800e631036bc5b9ab4a70b8909b3ac3c2a511
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2025-01-05 21:40:59 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { Helmet } from "react-helmet"; // 引入Helmet组件
+
 import Header from "./header"
 import "./layout.css"
 
@@ -24,6 +26,44 @@ const Layout = ({ children }) => {
 
   return (
     <>
+
+
+      {/* 使用Helmet组件管理头部信息 */}
+    <Helmet>
+      {/* 添加Google Analytics脚本（只添加一次） */}
+      <script src="https://www.google-analytics.com/analytics.js" async />
+      <script src="https://www.google-analytics.com/analytics.js" async />
+      <script src="https://www.google-analytics.com/analytics.js" async />
+      <script src="https://www.google-analytics.com/analytics.js" async />
+      {/* 添加自定义的JavaScript函数 */}
+      <script type="text/javascript">
+        {`
+          function show(x) {
+            var pubs = document.getElementsByClassName('pub');
+            for (var i=0; i<pubs.length; i++) {
+              if ((" "+pubs[i].className+" ").indexOf(" "+x+" ") >= 0) {
+                pubs[i].style.display = "inline";
+                pubs[i].style.opacity = '1';
+              } else {
+                pubs[i].style.display = "none";
+                pubs[i].style.opacity = '0';
+              }
+            }
+          }
+
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-76469747-1', 'auto');
+          ga('send', 'pageview');
+        `}
+      </script>
+    </Helmet>
+
+
+    
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
