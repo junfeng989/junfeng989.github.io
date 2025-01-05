eaa0a7b9267b4e25c2085f262a4d2adcd800e4de
junfeng989 <junfeng989@users.noreply.github.com>
2025-01-05 11:35:19 +0000

    deploy: b73555e267f4fcd95994ece700440db3a5930199

--- component---src-pages-404-js-83b010d37eca141a180a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{3331:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(6540),u=n(3895),l=n(2269);const s=()=>a.createElement(l.A,{title:"404: Not Found"});e.default=()=>a.createElement(u.A,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-83b010d37eca141a180a.js.map
