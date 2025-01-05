e2122422377b80c42ccbb0a6ea2dfb99d10679fe
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2025-01-05 13:59:44 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,25 @@ import { Link } from "gatsby"
 
 const Header = () => (
   <header style={headerStyles}>
+
+<script src="https://www.google-analytics.com/analytics.js" async="true"></script>
+<script src="https://www.google-analytics.com/analytics.js" async="true"></script>
+<script src="https://www.google-analytics.com/analytics.js" async="true"></script>
+<script src="https://www.google-analytics.com/analytics.js" async="true"></script>
+
+  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-76469747-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+
+  
   <title>Jun Feng's Homepage</title>
     <h1 style={{ textAlign: "left" }}>{/*安全AI*/}</h1>
     <nav>
