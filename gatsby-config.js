4a47c1248da5b940063033f4dbdbed628ede643a
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2025-01-06 13:20:48 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-react-helmet`，
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
