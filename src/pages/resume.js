ca80e4cdca1f91fef20eb3d2be4314c916cfe68d
Jun Feng <94858417+junfeng989@users.noreply.github.com>
2025-09-17 10:55:51 +0800

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -13,7 +13,7 @@ const ResumePage = () => (
       
       <h2>学术成果与荣誉</h2>
       <ul>
-        <li>发表重要学术论文40余篇，包括IEEE TDSC、IEEE TC、IEEE TIFS、AAAI、IEEE TII、ACM TOIT、IEEE TBD、IEEE TSUSC、IEEE IOT、INS、IEEE WCM等，
+        <li>发表重要学术论文50余篇，包括IEEE TDSC、IEEE TC、IEEE TIFS、NIPS、AAAI、IEEE TII、ACM TOIT、IEEE TBD、IEEE TSUSC、IEEE IOT、INS、IEEE WCM等，
             其中第一作者IEEE/ACM Transactions论文10余篇</li>
         <li>入选国家级博士后人才计划</li>
         <li>IEEE TCSC杰出博士论文奖获得者</li>
@@ -30,7 +30,8 @@ const ResumePage = () => (
         <li>主持国家自然科学基金青年项目“面向人机物系统的实用隐私保护张量深度学习研究”，2021-01-01 至 2023-12-31</li>
         <li>参与国家重点研发计划网络空间安全重点专项、国家自然科学基金重点项目</li>
         <li>担任国际会议IEEE ATC 2024、ICBC 2023、IEEE PriComp 2022、IEEE DependSys 2021、IEEE DependSys 2020、SpaCCS 2019的程序委员会主席</li>
-        <li>担任CCF A类会议NeurIPS 2025、ACM MM 2025、IJCAI 2025、ICML 2025、WWW 2025 (ACM TheWebConf 2025)、ICLR 2025、AAAI 2025、NeurIPS 2024、ACM MM 2024、AAAI 2024、AAAI 2023等顶级会议程序委员会委员或审稿人</li>
+        <li>担任CCF A类期刊IEEE Transactions on Information Forensics and Security (IEEE TIFS) 的Associate Editor</li>
+        <li>担任顶级会议ICLR 2026、AAAI 2026、ACSAC 2025、NeurIPS 2025、ACM MM 2025、IJCAI 2025、ICML 2025、WWW 2025 (ACM TheWebConf 2025)、ICLR 2025、AAAI 2025、NeurIPS 2024、ACM MM 2024、AAAI 2024、AAAI 2023等领域主席、程序委员会委员或审稿人</li>
         <li>担任20多个国际学术期刊例如IEEE TDSC、IEEE TIFS、IEEE TC、IEEE TMC、ACM TOSEM的审稿专家</li>
         <li>担任通信学报、CAAI Artificial Intelligence Research、网络与信息安全学报、网络空间安全科学学报等期刊青年编委</li>
         <li>担任电子学报、电子与信息学报、计算机研究与发展、China Communications等国内期刊的审稿专家</li>
@@ -45,7 +46,7 @@ const ResumePage = () => (
         <li>IEEE Transactions on Dependable and Secure Computing (CCF A, IF=7.3)</li>
         <li>IEEE Transactions on Information Forensics and Security (CCF A, IF=7.231, 中科院一区)</li>
         <li>IEEE Transactions on Computers (CCF A)</li>
-        <li>Transactions on Mobile Computing (CCF A)</li>
+        <li>IEEE Transactions on Mobile Computing (CCF A)</li>
         <li>ACM Transactions on Software Engineering and Methodology (CCF A)</li>
         <li>ACM Transactions on Internet Technology (CCF B, IF=3.989)</li>
         <li>IEEE Transactions on Industrial Informatics (IF=11.648, 中科院一区)</li>
